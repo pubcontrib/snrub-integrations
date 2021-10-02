992bde9694e7f4374dba1293bce1f3fa09d14dae
pubcontrib <pub.contrib@protonmail.com>
2021-10-02 15:26:30 -0500

    Reordered functions to more closely resemble the source script that is being overridden.

--- src/test.sh
+++ src/test.sh
@@ -61,6 +61,50 @@ fail()
     progress
 }
 
+conclude()
+{
+    end_seconds=`current_seconds`
+    total_seconds=$((end_seconds-start_seconds))
+
+    printf '\033[2K\015' 1>&2
+    printf '%d tests run. Took %d seconds.\n' $count $total_seconds
+    count=0
+}
+
+run_test()
+{
+    text=$1
+    expected_output=$2
+    expected_code=$3
+
+    actual_output=`$PROGRAM -t "$text"`
+    actual_code=$?
+
+    if [ $actual_code != $expected_code ]
+    then
+        printf '\033[2K\015' 1>&2
+        printf '[ERROR] Test case returned an unexpected exit code.\n' 1>&2
+        printf 'Hint: %s\n' "$hint" 1>&2
+        printf 'Source: %s\n' "$text" 1>&2
+        printf 'Expected: %d\n' $expected_code 1>&2
+        printf 'Actual: %d\n' $actual_code 1>&2
+        exit 1
+    fi
+
+    if [ "$actual_output" != "$expected_output" ]
+    then
+        printf '\033[2K\015' 1>&2
+        printf '[ERROR] Test case returned an unexpected stdout.\n' 1>&2
+        printf 'Hint: %s\n' "$hint" 1>&2
+        printf 'Source: %s\n' "$text" 1>&2
+        printf 'Expected: %s\n' "$expected_output" 1>&2
+        printf 'Actual: %s\n' "$actual_output" 1>&2
+        exit 1
+    fi
+
+    count=$((count + 1))
+}
+
 run_batch_test()
 {
     text=$1
@@ -178,40 +222,6 @@ run_memory_test()
     count=$((count + 1))
 }
 
-run_test()
-{
-    text=$1
-    expected_output=$2
-    expected_code=$3
-
-    actual_output=`$PROGRAM -t "$text"`
-    actual_code=$?
-
-    if [ $actual_code != $expected_code ]
-    then
-        printf '\033[2K\015' 1>&2
-        printf '[ERROR] Test case returned an unexpected exit code.\n' 1>&2
-        printf 'Hint: %s\n' "$hint" 1>&2
-        printf 'Source: %s\n' "$text" 1>&2
-        printf 'Expected: %d\n' $expected_code 1>&2
-        printf 'Actual: %d\n' $actual_code 1>&2
-        exit 1
-    fi
-
-    if [ "$actual_output" != "$expected_output" ]
-    then
-        printf '\033[2K\015' 1>&2
-        printf '[ERROR] Test case returned an unexpected stdout.\n' 1>&2
-        printf 'Hint: %s\n' "$hint" 1>&2
-        printf 'Source: %s\n' "$text" 1>&2
-        printf 'Expected: %s\n' "$expected_output" 1>&2
-        printf 'Actual: %s\n' "$actual_output" 1>&2
-        exit 1
-    fi
-
-    count=$((count + 1))
-}
-
 progress()
 {
     percent=`printf "scale=0; $case*100/$CASES\n" | bc`
@@ -240,16 +250,6 @@ progress()
 
         last_percent=$percent
     fi
-}
-
-conclude()
-{
-    end_seconds=`current_seconds`
-    total_seconds=$((end_seconds-start_seconds))
-
-    printf '\033[2K\015' 1>&2
-    printf '%d tests run. Took %d seconds.\n' $count $total_seconds
-    count=0
 }
 
 current_seconds()
