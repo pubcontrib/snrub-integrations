1e5f447caffd96d4f281b056c3e5f09233554375
pubcontrib <pub.contrib@protonmail.com>
2021-11-10 21:33:27 -0600

    Added a test suite for real file operators.

--- src/test.sh
+++ src/test.sh
@@ -1,5 +1,24 @@
 hint='unset'
 
+file_suite()
+{
+    hint 'integrations/file'
+
+    pass '`1` ("[o]->" "/tmp/file.txt")' '?'
+    pass '("[o]<-" "/tmp/file.txt" "created")' '?'
+    pass '`2` ("[o]->" "/tmp/file.txt")' '"created"'
+    pass '("[o]<-" "/tmp/file.txt" "updated")' '?'
+    pass '`3` ("[o]->" "/tmp/file.txt")' '"updated"'
+    pass '("[o]--" "/tmp/file.txt")' '?'
+    pass '`4` ("[o]->" "/tmp/file.txt")' '?'
+    pass '("x<-" "before" ("[o]->" "/tmp/file.txt"))
+    ("[o]<-" "/tmp/file.txt" "temporary")
+    ("x<-" "during" ("[o]->" "/tmp/file.txt"))
+    ("[o]--" "/tmp/file.txt")
+    ("x<-" "after" ("[o]->" "/tmp/file.txt"))
+    [("x->" "before") ("x->" "during") ("x->" "after")]' '[? "temporary" ?]'
+}
+
 hint()
 {
     hint=$1
@@ -77,6 +96,8 @@ introduce()
 
 conclude()
 {
+    file_suite
+
     end_seconds=`current_seconds`
     total_seconds=`expr $end_seconds - $start_seconds`
 
