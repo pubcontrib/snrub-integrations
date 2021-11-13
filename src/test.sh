3256c858ae6c19c2dbae95d3b6e676dd7ac4a6e2
pubcontrib <pub.contrib@protonmail.com>
2021-11-13 10:23:27 -0600

    Fixed builtin test cases not being included in the progress count.

--- src/test.sh
+++ src/test.sh
@@ -1,24 +1,5 @@
 hint='unset'
 
-file_suite()
-{
-    hint 'integrations/file'
-
-    pass '`1` ("[o]->" "/tmp/file.txt")' '?'
-    pass '("[o]<-" "/tmp/file.txt" "created")' '?'
-    pass '`2` ("[o]->" "/tmp/file.txt")' '"created"'
-    pass '("[o]<-" "/tmp/file.txt" "updated")' '?'
-    pass '`3` ("[o]->" "/tmp/file.txt")' '"updated"'
-    pass '("[o]--" "/tmp/file.txt")' '?'
-    pass '`4` ("[o]->" "/tmp/file.txt")' '?'
-    pass '("x<-" "before" ("[o]->" "/tmp/file.txt"))
-    ("[o]<-" "/tmp/file.txt" "temporary")
-    ("x<-" "during" ("[o]->" "/tmp/file.txt"))
-    ("[o]--" "/tmp/file.txt")
-    ("x<-" "after" ("[o]->" "/tmp/file.txt"))
-    [("x->" "before") ("x->" "during") ("x->" "after")]' '[? "temporary" ?]'
-}
-
 hint()
 {
     hint=$1
@@ -96,7 +77,7 @@ introduce()
 
 conclude()
 {
-    file_suite
+    run_integration_tests
 
     end_seconds=`current_seconds`
     total_seconds=`expr $end_seconds - $start_seconds`
